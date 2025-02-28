a511da7c184eb7e81c13a7eb45af3e33e873aa8b
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-02-28 23:15:40 +0300

    proceeding with the project with planning in trello for better development experience

--- venv/project/app1/models.py
+++ venv/project/app1/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 import datetime
 # Create your models here.
+
+
+# Model for the Users of the system. Hospital staff
+
 class Patientdata(models.Model):
     first_name= models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
