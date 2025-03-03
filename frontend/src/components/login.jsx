c65706e8cc32eb221e07027916710c073bc1d6ce
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-03-03 16:41:47 +0300

    figma designs done , working on the sign up and log in screens

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,5 +1,9 @@
+import styles from "./styles.module.css";
+
 export default function Login() {
-    return <div>
-        <p>Login</p>
+  return (
+    <div>
+      <p>Login</p>
     </div>
+  );
 }
