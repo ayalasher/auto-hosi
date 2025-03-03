a4981faf7b1f12e6527c92951e70b0957a634a42
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-03-03 17:44:09 +0300

    login and sign up UI done, adding funtionality next

--- frontend/src/components/Signuo.jsx
+++ frontend/src/components/Signuo.jsx
@@ -1,7 +1,107 @@
+import styles from "./styles.module.css";
+import { FaHeartCirclePlus } from "react-icons/fa6";
+import { CiUser } from "react-icons/ci";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaUserMd } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
 export default function Signuo() {
   return (
-    <div>
-      <p>Sign up</p>
+    <div className={styles.loginscreen}>
+      <h1>
+        Auto Hosi System <FaHeartCirclePlus size={25} />{" "}
+      </h1>
+      <h3>Sign up</h3>
+      <form className={styles.loginform} action="submit">
+        <fieldset className={styles.fieldset}>
+          <label className={styles.labels} htmlFor="">
+            User name
+          </label>
+          <br />
+          <div className={styles.iconinputcontainer}>
+            <CiUser size={20} color="#7f8c98" />
+            <input
+              className={styles.inputs}
+              type="text"
+              //   value=""
+              placeholder="Enter your  name"
+              color="green"
+              required
+            />
+          </div>
+
+          <br />
+
+          <label className={styles.labels} htmlFor="">
+            User Email
+          </label>
+          <br />
+          <div className={styles.iconinputcontainer}>
+            <MdEmail size={20} color="#7f8c98" />
+            <input
+              className={styles.inputs}
+              type="text"
+              //   value=""
+              placeholder="Enter your Email"
+              color="green"
+              required
+            />
+          </div>
+
+          <br />
+
+          <label className={styles.labels} htmlFor="">
+            User role
+          </label>
+          <br />
+          <div className={styles.iconinputcontainer}>
+            <FaUserMd size={20} color="#7f8c98" />
+            <input
+              className={styles.inputs}
+              type="text"
+              //   value=""
+              placeholder="Enter your role"
+              required
+            />
+          </div>
+
+          <br />
+          <label className={styles.labels} htmlFor="">
+            Password
+          </label>
+          <br />
+          <div className={styles.iconinputcontainer}>
+            <RiLockPasswordFill size={20} color="#7f8c98" />
+            <input
+              className={styles.inputs}
+              type="password"
+              //   value=""
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <br />
+
+          <label className={styles.labels} htmlFor="">
+            Confirm Password
+          </label>
+          <br />
+          <div className={styles.iconinputcontainer}>
+            <RiLockPasswordFill size={20} color="#7f8c98" />
+            <input
+              className={styles.inputs}
+              type="password"
+              //   value=""
+              placeholder="Confirm your password"
+              required
+            />
+          </div>
+          <br />
+        </fieldset>
+        <button className={styles.loginsignupbutton} type="submit">
+          Login
+        </button>
+      </form>
     </div>
   );
 }
