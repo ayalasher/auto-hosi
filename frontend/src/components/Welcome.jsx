289be2b6fa46215a3a7b0c3d61fefc22efae754c
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-03-04 17:50:12 +0300

    top section complete... adding a dashboard

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,10 +1,54 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { FaHeartCirclePlus } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+import { MdLogout } from "react-icons/md";
+import styles from "./styles.module.css";
 export default function Welcome() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // get the user data from the log in and the sign up page....
+  const { userdata } = location.state || {};
   return (
-    <div>
-      <p>Welcome screen</p>
+    <div className={styles.welcomescreen}>
+      <div className={styles.topsection}>
+        <h1 className={styles.heading}>
+          Auto hosi system {"   "} <FaHeartCirclePlus size={25} />{" "}
+        </h1>
+        <div className={styles.somethinggood}>
+          <div className={styles.sgc1}>
+            <p className={styles.heading2}>Welcome </p>
+            <p>
+              <b>Username:</b>
+            </p>
+            <p> {userdata.username} </p>
+            <p className={styles.textstyling}>
+              <b>user role:</b>
+            </p>
+            <p> {userdata.role} </p>
+          </div>
+
+          <div className={styles.sgc2}>
+            <button
+              // onClick={alert("hakuna mchezo raondiii")}
+              className={styles.topbuttons}
+            >
+              {" "}
+              <FaUser color={"#CBCBCB"} size={25} />{" "}
+            </button>
+            <button
+              // onClick={alert("hakuna mchezo raondiii")}
+              className={styles.topbuttons}
+            >
+              {" "}
+              <MdLogout color={"#CBCBCB"} size={25} />{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* <hr color="green" size="2" /> */}
     </div>
   );
 }
