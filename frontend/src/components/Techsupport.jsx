c6b4382bf777e6bcaa22332699f7ea77341464d6
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-03-10 23:12:40 +0300

    Tech support page done

--- frontend/src/components/Techsupport.jsx
+++ frontend/src/components/Techsupport.jsx
@@ -1,9 +1,17 @@
 import styles from "./styles.module.css";
+import { MdOutlineContactPhone } from "react-icons/md";
 
 export default function Techsupport() {
   return (
-    <div>
-      <p> tech Support</p>
+    <div className={styles.techsupportsection} >
+      {/* <p> tech Support</p> */}
+      <h3>Contact me</h3>
+      <MdOutlineContactPhone size={25} color="black" />
+      <p>0745405309</p>
+      <div className={styles.footerstyling} >
+        <div className={styles.footercontent} ><p>
+          <b>Developed by :</b> maxwel ayal asher © 2025 </p></div>
+      </div>
     </div>
   );
 }
